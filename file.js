eccdf45025c68d48ef4c45c4b5d144d893b44ae9
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-23 00:34:16 +0530

    practice

--- file.js
+++ file.js
@@ -1,6 +1,25 @@
+const { time, timeLog } = require("console");
 const fs =require("fs");
 
-fs.writeFileSync("./test.txt", "i a creating a file for first time");
-fs.writeFileSync("./test.txt", "we want editing");
+// fs.writeFileSync("./test.txt", "i a creating a file for first time");
+// fs.writeFileSync("./test.txt", "we want editing");
 
-fs.writeFile("./test.txt", "thhis is async", (error)=>{})
+// fs.writeFile("./test.txt", "thhis is async", (error)=>{})
+
+// console.log(fs.readFileSync("./contact.txt", "utf-8"))
+
+// fs.readFile("./contact.txt", "utf-8", (err, result)=>{
+//     if (err) {
+//         console.log("error"  + err)
+//     }else{
+//         console.log(result)
+//     }
+// })
+
+// fs.appendFileSync("./test.txt", new Date().getMinutes().toLocaleString()+"\n");
+// console.log(new Date().getDate().toLocaleString())
+// fs.cpSync("./test.txt", "./tesstt.txt")
+// fs.unlinkSync("tesstt.txt")
+// console.log(fs.statSync("./test.txt"))
+// fs.mkdirSync("my-docs")
+// fs.mkdirSync("my-docs/a/dx", {recursive: true})
