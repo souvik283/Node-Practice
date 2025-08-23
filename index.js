1ae836e48e199523320b259b2fd46ea4c00e9f4d
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-24 02:02:17 +0530

    practice

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myserver = http.createServer((req, res) => {
+        if(req.url == "/favicon.ico"){
+        return res.end();
+    }
+    let myurl = url.parse(req.url, true)
+    console.log(myurl)
     let log = `New request is recieved from ${req.url} at ${new Date().getHours().toLocaleString()} : ${new Date().getMinutes().toLocaleString()} Date: ${new Date().getDate().toLocaleString()}/${new Date().getMonth().toLocaleString()}/${new Date().getFullYear().toLocaleString()}`
+
     fs.appendFile("test.txt", `\n ${log}`, (err, result) => {
-        switch (req.url) {
+        switch (myurl.pathname) {
             case "/":
-                res.end("Welcome To Homepage")
+                const name = myurl.query.name
+                res.end(`Welcome ${name} and have a id: ${myurl.query.id}`)
                 break;
-            case "/about":
-                res.end("Welcome To About page Server")
+            case "/search":
+                let searchItem = myurl.query.search_item
+                res.end(searchItem)
                 break;
             default:
                 res.end("404 Not Found")
